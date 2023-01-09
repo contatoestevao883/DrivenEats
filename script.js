b5446292e9b3a00a62f2cda5842d74dfc69aad74
Estevão <contatoestevao883@hotmail.com>
2023-01-09 16:06:14 -0300

    corrigindo bugs

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const plate = document.querySelector('.prato').innerText
+const price = document.querySelector('h3').textContent
+const drink = document.querySelector('.drink').innerText
+const dessert = document.querySelector('.dessert').innerText
 
 function itemSelected(pedidoClicado){
-
     const selected = document.querySelector('.selected')
+    const plate1 = document.querySelector(".plate1")
 
     if(selected !== null){
         selected.classList.remove('selected')
     }
     const buttonSelected = document.querySelector(pedidoClicado)
+    let plateSelected =  document.querySelector(pedidoClicado)
     buttonSelected.classList.add('selected')
+    const plateSelected1 = plate1.innerText = plateSelected.innerText
     buttonActive()
 }
 
@@ -57,20 +62,22 @@ function buttonActive(){
 
 function showModal(){
     const modal = document.querySelector(".modal")
+    const container = document.querySelector(".container")
+    const plate1 = document.querySelector(".plate1")
+    const drink1 = document.querySelector(".drink1")
+    const dessert1 = document.querySelector(".dessert1")
     const main = document.querySelector('main')
     const header = document.querySelector("header")
     const button = document.querySelector("button")
     const pedido1 = document.createElement('h3')
+    const pedido2 = document.createElement('h3')
+    const pedido3 = document.createElement('h3')
+    const price1 = document.createElement("span")
 
 
     modal.classList.remove("hidden")
     button.style.opacity = '5%'
     main.style.opacity = '5%'
     header.style.opacity = '5%'
-
-    
-    pedido1.innerText = plate
-
-    modal.appendChild(pedido1)
 
 }
