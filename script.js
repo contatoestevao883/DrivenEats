7d34841e5cc51722b1ee194a871c94dbee4a244e
Estevão <contatoestevao883@hotmail.com>
2023-01-09 00:18:43 -0300

    Parte do select/checkbox/botão concluídas

--- script.js
+++ script.js
@@ -1,54 +1,40 @@
 function itemSelected(pedidoClicado){
 
     const selected = document.querySelector('.selected')
+
     if(selected !== null){
         selected.classList.remove('selected')
     }
     const buttonSelected = document.querySelector(pedidoClicado)
     buttonSelected.classList.add('selected')
-    checkBox()
 }
-function checkBox(){
-    const element = document.querySelector('ion-icon').style.display 
-    if(element === 'none'){
-        document.querySelector('ion-icon').style.display = 'block'
-    }else{
-        document.querySelector('ion-icon').style.display = 'none'
-    }
-    console.log(element)
-}
+
 
 
 function itemSelectedDrinks(pedidoClicado){
-    console.log(pedidoClicado)
-
-    const hidden = document.querySelector('.hidden')
-    console.log(hidden)
 
     const selectedDrinks = document.querySelector('.selectedDrinks')
-    console.log(selectedDrinks)
     
     if(selectedDrinks !== null ){
         selectedDrinks.classList.remove('selectedDrinks')
     }
     const buttonSelected = document.querySelector(pedidoClicado)
     buttonSelected.classList.add('selectedDrinks')
-    console.log(hidden.classList.add('hidden'))
+   
 }
 
 function itemSelectedDesert(pedidoClicado){
-    console.log(pedidoClicado)
-
-    const hidden = document.querySelector('.hidden')
-    console.log(hidden)
-
+   
     const selectedDesert = document.querySelector('.selectedDesert')
-    console.log(selectedDesert)
-    
+    const button = document.querySelector('button')
+
     if(selectedDesert !== null ){
         selectedDesert.classList.remove('selectedDesert')
     }
     const buttonSelected = document.querySelector(pedidoClicado)
     buttonSelected.classList.add('selectedDesert')
-    console.log(hidden.classList.add('hidden'))
+    button.removeAttribute('disabled')
+        button.innerText = 'Fechar o pedido'
+        button.style.background = '#32B72F'
+        button.style.color = 'white'
 }
