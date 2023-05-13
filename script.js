7678a42411870d82ec3f01da61df980ef56761b0
contatoestevao883 <contatoestevao883@hotmail.com>
2023-05-12 21:33:10 -0300

    feat: Adicionando modal estático para compra

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const plate = document.querySelector('.prato').innerText
-const price = document.querySelector('h3').textContent
-const drink = document.querySelector('.drink').innerText
-const dessert = document.querySelector('.dessert').innerText
+// const plate = document.querySelector('.prato').innerText
+// const price = document.querySelector('h3').textContent
+// const drink = document.querySelector('.drink').innerText
+// const dessert = document.querySelector('.dessert').innerText
+
+let titlePlate, titleDrink, titleDesert;
+let pricePlate, priceDrink, priceDesert;
 
 function itemSelected(pedidoClicado){
     const selected = document.querySelector('.selected')
-    const plate1 = document.querySelector(".plate1")
 
     if(selected !== null){
         selected.classList.remove('selected')
@@ -13,7 +15,6 @@ function itemSelected(pedidoClicado){
     const buttonSelected = document.querySelector(pedidoClicado)
     let plateSelected =  document.querySelector(pedidoClicado)
     buttonSelected.classList.add('selected')
-    const plateSelected1 = plate1.innerText = plateSelected.innerText
     buttonActive()
 }
 
@@ -49,7 +50,7 @@ function buttonActive(){
     const selected = document.querySelector('.selected')
     const selectedDrinks = document.querySelector('.selectedDrinks')
     const selectedDesert = document.querySelector('.selectedDesert')
-    const button = document.querySelector('button')
+    const button = document.querySelector('.final-button')
     
     if(selected !== null && selectedDrinks !== null && selectedDesert !== null){
         button.disabled = false
@@ -61,23 +62,30 @@ function buttonActive(){
 
 
 function showModal(){
-    const modal = document.querySelector(".modal")
-    const container = document.querySelector(".container")
-    const plate1 = document.querySelector(".plate1")
-    const drink1 = document.querySelector(".drink1")
-    const dessert1 = document.querySelector(".dessert1")
-    const main = document.querySelector('main')
-    const header = document.querySelector("header")
-    const button = document.querySelector("button")
-    const pedido1 = document.createElement('h3')
-    const pedido2 = document.createElement('h3')
-    const pedido3 = document.createElement('h3')
-    const price1 = document.createElement("span")
+    const modal = document.querySelector('.modal');
+    modal.classList.remove('hidden');
+
+    const titlePlate = document.querySelector('prato .nome');
+    titlePlate.innerHTML = titlePlate;
+    document.querySelector('.prato .preco').innerHTML = pricePlate;
+
+    const titleDrink = document.querySelector('.pedido .nome');
+    titleDrink.innerHTML = titleDrink;
+    document.querySelector('.prato .preco').innerHTML = priceDrink;
+
+    const titleDesert = document.querySelector('.pedido .nome');    
+    titleDesert.innerHTML = titleDesert;    
+    document.querySelector('.prato .preco').innerHTML = priceDesert;
 
 
-    modal.classList.remove("hidden")
-    button.style.opacity = '5%'
-    main.style.opacity = '5%'
-    header.style.opacity = '5%'
+    total = pricePlate + priceDrink + priceDesert;
+    console.log(total)
 
+    document.querySelector('.preco-total').innerHTML = total.toFixed(2);
+
+}
+
+function cancel(){
+    const overlay = document.querySelector('.modal');
+    overlay.classList.add('hidden');
 }
